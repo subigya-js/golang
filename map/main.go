cadd69f2711f4e5bcfc5e8bb031603effbe9096d
subigya-js <developer.subigya@gmail.com>
2025-04-03 22:04:49 +0530

    add examples of deleting keys and checking existence in map usage in main.go

--- map/main.go
+++ map/main.go
@@ -15,9 +15,33 @@ func main() {
 	// }
 	studentGrade["Garima"] = 85
 	studentGrade["Subigya"] = 95
+	studentGrade["Gaurav"] = 90
 
-	fmt.Println(studentGrade)
+	fmt.Println("Student Grade before deleting:", studentGrade)
+
+	// Deleting Gaurav
+	delete(studentGrade, "Gaurav")
+	fmt.Println("Student Grade after deleting:", studentGrade)
 
 	// Accessing marks of Garima
 	fmt.Println("Marks of Garima is:", studentGrade["Garima"])
+
+	// To check if a key exists
+	grades, exists := studentGrade["Ronaldo"]
+	fmt.Println("Grades of David is:", grades)
+	fmt.Println("Does David exist in the map?", exists)
+
+	for i, value := range studentGrade {
+		fmt.Println("Grades of", i, "is:", value)
+	}
+
+	studentGrades := map[string] int {
+		"Alice": 90,
+		"Bob": 85,
+		"Charlie": 95,
+	}
+	
+	for i, value:=range studentGrades {
+		fmt.Println("Grades of", i, "is:", value)
+	}
 }
